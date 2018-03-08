21740fa567e08758f2910ad62f6a1e85d9be21fe
Evan King <Ares513@users.noreply.github.com>
2018-03-07 19:02:14 -0500

    Wrangling is almost done. The vectors from spacy are huge so I need to break it up across more files.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,3 @@
-import keras
 import emoji
 import re
 import csv
@@ -7,27 +6,38 @@ import datetime
 import time
 import numpy as np
 
-
+import cnn
 emojis_list = map(lambda x: ''.join(x.split()), emoji.UNICODE_EMOJI.keys())
 r = re.compile('|'.join(re.escape(p) for p in emojis_list))
+
+
 def filterEmojis(s):
     s = re.sub(r, ' ', s)
     return s
 
 
-def filterEmojisFromFile(path):
+def buildInputArrays(path, nlp):
     output = []
-    filtered = []
 
     with open(path, 'r', encoding="utf8") as csvfile:
+        print("Tokenizing file at " + path)
         reader = csv.DictReader(csvfile)
+        rowCount = 0
         for row in reader:
-            output.append((row['text'], row['userid']))
+            rowCount += 1
+            if rowCount % 10000 == 0:
+                print("Completed a batch of 10000 rows. Current row count: " + str(rowCount))
+            rowTokens = []
+            tokens = nlp(row['text'])
+            for tok in tokens:
+                rowTokens.append(tok.vector)
+            output.append((rowTokens, row['userid']))
     print(datetime.datetime.now())
     return output
 
 
 def preprocessInput(path):
+    """"Removes emojis and writes them out to file."""
     lines = []
 
     with open(path, 'r') as inputFile:
@@ -37,32 +47,33 @@ def preprocessInput(path):
         for line in lines:
             outputFile.write(filterEmojis(line))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def writeOutFile(array, filename, max_terms_per_file):
+    
 
 
 if __name__ == "__main__":
-    now = time.time()
+    now = datetime.datetime.now()
     print("Preprocessing begins at " + str(now))
+    print("Loading Spacy's large vector set. " + str(now))
     nlp = spacy.load('en_vectors_web_lg')
-    bot_tweets = filterEmojisFromFile('../data/bot_tweets.csv')
-    bot_labels = np.ones(len(bot_tweets), dtype=int)
-    regular_tweets = []
+    now = datetime.datetime.now()
+    print("Finished loading. " + str(now))
+
+    bot_tweets = buildInputArrays('../data/bot_tweets.csv', nlp)
+    #bot_labels = np.ones(len(bot_tweets), dtype=int)
+    #we can load the labels later, since they're trivial to infer from the file we load
+    with open("bot_vectorized.npy", 'wb') as bot_tweet_file:
+        bot_tweet_np = np.array(bot_tweets)
+        np.save(bot_tweet_file, bot_tweet_np)
 
     # a bit brutish but I don't want to deal with a lot of file IO
     for chunkNumber in range(0, 7):
-        #regular_tweets += filterEmojisFromFile('../data/non_bot_chunk_' + str(chunkNumber) + '.csv')
-        preprocessInput('../data/non_bot_chunk_' + str(chunkNumber) + '.csv')
-    regular_labels = np.zeros(len(regular_tweets), dtype=int)
+        print("Beginning file " + str(chunkNumber))
+        matrix_tweets = []
+        matrix_tweets += buildInputArrays('../data/non_bot_chunk_' + str(chunkNumber) + '.csv', nlp)
+        with open("ordinary_vectorized_" + str(chunkNumber) + ".npy", 'wb') as ordinary_tweet_file:
+            ordinary_tweet_np = np.array(matrix_tweets)
+            np.save(ordinary_tweet_file, ordinary_tweet_np)
+    now = datetime.datetime.now()
     print("Preprocessing complete at " + str(now))
+
