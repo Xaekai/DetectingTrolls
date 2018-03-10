d3f862f24511bbe93156735073b5c3dd85f10c16
Evan King <Ares513@users.noreply.github.com>
2018-03-10 04:55:30 -0500

    FINALLY GOT THE MODEL TRAINING ON SOMETHING EVEN IF IT'S DUMB

--- src/FileReader.py
+++ src/FileReader.py
@@ -3,35 +3,83 @@ import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 
-
+MAX_VECTOR_COUNT = 70*300 # word2vecs are 300 length vectors
 def process():
     print("Loading data")
 
-    regular_tweets = []
-    bot_tweets = []
+    bot_tweets, bot_labels, regular_tweets, regular_labels = load_10_regular_1_bot()
+    print("Done loading")
+    all_tweets = np.append(regular_tweets, bot_tweets)
+    all_labels = np.append(regular_labels, bot_labels)
+    # and now, because doing it in preprocessing would make the files even more ridiculously huge, we're going to pad to 50 with zeroes.
+    all_adjusted = []
+    for tweet in all_tweets:
+        if len(tweet) >  MAX_VECTOR_COUNT:
+            tweet = tweet[:MAX_VECTOR_COUNT]
+            #trim, but it's highly unlikely there are that many tokens
+            print("Trimming tweet vectors with vector length " + str(len(tweet)))
+        else:
+            # we need to add fake empty tokens to pad
+            needed_empty = MAX_VECTOR_COUNT - len(tweet)
+            tweet = padarray(tweet, MAX_VECTOR_COUNT)
+            print(len(tweet))
+        all_adjusted.append(tweet)
+
+    # split it once on training / test
+    #x, x_test, y, y_test = train_test_split(all_tweets, all_labels, test_size=0.25, train_size=0.75)
+    # split training again on validation
+    #x_train, x_cv, y_train, y_cv = train_test_split(x, y, test_size=0.2, train_size=0.8)
+
+    # print("Test size" + str(len(x_test)))
+    # print("Train size " + str(len(x_train)))
+    # print("Validation size " + str(len(x_cv)))
+    # print("Total size " + str(len(all_tweets)))
+    # print("Test percent " + str(len(x_test) / len(all_tweets)))
+    # print("Train percent " + str(len(x_train) / len(all_tweets)))
+    # print("Validation percent " + str(len(x_cv) / len(all_tweets)))
+    return (None, None, None, None, None, None, None, None, np.array(all_adjusted), all_labels)
+
+def padarray(A, size):
+    t = size - len(A)
+    return np.pad(A, pad_width=(0, t), mode='constant')
+
+def load_10_regular_1_bot():
+    """Loads files such that roughly 90% are regular and 10% are bot tweets. Takes roughly 6.5gb RAM"""
+    bot_loaded = np.load("D:/data/bot_tweets_vectorized_0" + ".npy")
+    bot_tweets_unfiltered = [x[0] for x in bot_loaded]
+    regular_tweets_unfiltered = []
+    for chunkNo in range(0, 1):
+        print("Loading " + "D:/data/regular_tweets_vectorized_0" + "_" + str(chunkNo) + ".npy")
+        regular_loaded = np.load("D:/data/regular_tweets_vectorized_0" + "_" + str(chunkNo) + ".npy")
+        regular_tweets_unfiltered = np.append(regular_tweets_unfiltered, [x[0] for x in regular_loaded])
+    bot_tweets = list(filter(lambda x: len(x) != 0, bot_tweets_unfiltered))
+    regular_tweets = list(filter(lambda x: len(x) != 0, regular_tweets_unfiltered))
+    bot_tweets = [np.concatenate(x).ravel() for x in bot_tweets]
+    regular_tweets = [np.concatenate(x).ravel() for x in regular_tweets]
+    bot_labels = np.ones(len(bot_tweets), dtype=int)
+    regular_labels = np.zeros(len(regular_tweets), dtype=int)
+    return (bot_tweets, bot_labels, regular_tweets, regular_labels)
+
+
+def load_all():
+    """WARNING: This takes roughly 45 GB of RAM to do in entirety."""
+    """Totally impractical right now without something like Spark"""
+    bot_tweets_unfiltered = []
+    regular_tweets_unfiltered = []
     for fileNo in range(0, 9):
         print("Loading " + "D:/data/bot_tweets_vectorized_" + str(fileNo) + ".npy")
         bot_loaded = np.load("D:/data/bot_tweets_vectorized_" + str(fileNo) + ".npy")
-        bot_tweets = np.append(bot_tweets, [(x[0], 1) for x in bot_loaded])
+        bot_tweets_unfiltered = np.append(bot_tweets_unfiltered, [x[0] for x in bot_loaded])
         for chunkNo in range(0, 9):
             print("Loading " + "D:/data/regular_tweets_vectorized_" + str(fileNo) + "_" + str(chunkNo) + ".npy")
             regular_loaded = np.load("D:/data/regular_tweets_vectorized_" + str(fileNo) + "_" + str(chunkNo) + ".npy")
-            regular_tweets = np.append(regular_tweets, [(x[0], 1) for x in regular_loaded])
-    print("Done loading")
-    all_tweets = np.append(regular_tweets, bot_tweets)
-    # split it once on training / test
-    x, x_test, y, y_test = train_test_split(all_tweets, test_size=0.25, train_size=0.75)
-    # split training again on validation
-    x_train, x_cv, y_train, y_cv = train_test_split(x, y, test_size=0.2, train_size=0.8)
+            regular_tweets_unfiltered = np.append(regular_tweets_unfiltered, [x[0] for x in regular_loaded])
+    bot_tweets = list(filter(lambda x: len(x) != 0, bot_tweets_unfiltered))
+    regular_tweets = list(filter(lambda x: len(x) != 0, regular_tweets_unfiltered))
 
-    print("Test size" + str(len(x_test)))
-    print("Train size " + str(len(x_train)))
-    print("Validation size " + str(len(x_cv)))
-    print("Total size " + str(len(data)))
-    print("Test percent " + str(len(x_test) / len(data)))
-    print("Train percent " + str(len(x_train) / len(data)))
-    print("Validation percent " + str(len(x_cv) / len(data)))
-    return (x, x_test, x_train, x_cv, y, y_test, y_train, y_cv)
+    bot_labels = np.ones(len(bot_tweets), dtype=int)
+    regular_labels = np.zeros(len(regular_tweets), dtype=int)
+    return (bot_tweets, bot_labels, regular_tweets, regular_labels)
 
 
 if __name__ == "__main__":
---
+++ src/cnn.py
@@ -0,0 +1,65 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten
+import numpy as np
+import time
+import FileReader
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+from keras.utils import to_categorical
+import sklearn.metrics as metrics
+
+
+def execute_cnn():
+    # Model Template
+    (x, x_test, x_train, x_cv, y, y_test, y_train, y_cv, all_tweets, all_labels) = FileReader.process()
+    # y = to_categorical(y)
+    # y_train = to_categorical(y_train)
+    # y_test = to_categorical(y_test)
+    print(all_tweets[0])
+    print(len(all_tweets[0]))
+    for val in all_tweets:
+        print(len(val))
+    adjusted = to_categorical(all_labels)
+    model = Sequential() # declare model
+    model.add(Dense(10, input_shape=(21000, ), kernel_initializer='he_normal')) # first layer
+    model.add(Activation('relu'))
+    model.add(Dense(5096, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(5096, activation='relu'))
+    model.add(Dense(5096, activation='sigmoid'))
+    model.add(Dense(5096, activation='relu'))
+    model.add(Dense(2, kernel_initializer='he_normal')) # last layer
+    model.add(Activation('softmax'))
+    model.summary()
+    # Compile Model
+    model.compile(optimizer='sgd',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    # Train Model
+    history = model.fit(all_tweets, adjusted,
+                        validation_data = (all_tweets, adjusted),
+                        epochs=1,
+                        batch_size=512,
+                        verbose=1)
+    # Report Results
+    predicted = model.predict(all_tweets)
+    n_values = 10
+    y_pred = np.eye(n_values, dtype=int)[np.argmax(predicted, axis=1)]
+    confusion = confusion_matrix(y_cv.argmax(axis=1), y_pred.argmax(axis=1))
+    acc = metrics.accuracy_score(y_cv, y_pred)
+    prec = metrics.precision_score(y_cv, y_pred, average=None)
+    rec = metrics.recall_score(y_cv, y_pred, average=None)
+    whoops = [] #figure out which ones were misclassified, dubbed the "whoops array"
+    for val in range(0, len(y_pred)):
+        if(np.array_equal(y_cv[val], y_pred[val])):
+            pass
+        else:
+            whoops.append((y_pred[val], x[val]))
+
+    print(confusion)
+    print(acc)
+    print(rec)
+    print(prec)
+
+if __name__ == "__main__":
+    execute_cnn()
