9338bb78bfe1217079c9fff7085ca9a98787dd7f
Evan King <Ares513@users.noreply.github.com>
2018-04-30 22:49:47 -0400

    Model update and refactoring complete. Now achieving 96% accuracy, 99% recall, 95% precisiongit status

--- src/global_processor.py
+++ src/global_processor.py
@@ -9,93 +9,8 @@ import os
 from sklearn.externals import joblib
 import logging
 import sys
-
-
-def build_large_arr(base_name, bot_no, chunk_lower, chunk_higher, fileNo):
-    """Completes preprocessing by loading in a few chunks, normalizing them across zero mean
-    and saving them to npy files. The network will create and load these.
-    """
-    print("Loading data")
-    print("")
-    bot_tweets, bot_labels, regular_tweets, regular_labels = load_chunk(bot_no, chunk_lower, chunk_higher, fileNo)
-    print("Done loading")
-    all_tweets = np.append(regular_tweets, bot_tweets)
-    all_labels = np.append(regular_labels, bot_labels)
-    del bot_tweets, regular_tweets, regular_labels, bot_labels
-    all_adjusted = pad_tweets(all_tweets)
-    del all_tweets
-    print("Adjustment complete. Scaling...")
-    # split it once on training / test
-    print("Saving data...")
-    with open("../data/" + base_name +"_tweets" + ".npy", 'wb') as outputFile:
-        np.save(outputFile, all_adjusted)
-    with open("../data/" + base_name + "_labels" + ".npy", 'wb') as outputFile:
-        np.save(outputFile, all_labels)
-
-
-def process(large_file_path_to_load, labels_path):
-    print("Loading data from large file...")
-    all_adjusted = np.load(large_file_path_to_load + ".npy")
-    all_labels = np.load(labels_path + ".npy")
-
-    print("Loaded. Scaling data...")
-    scaler = StandardScaler(copy=False)
-    all_scaled = scaler.fit_transform(all_adjusted)
-    print("Scaling complete. Performing model selection...")
-    x_train, x_test, y_train, y_test = train_test_split(all_scaled, all_labels, shuffle=False, test_size=0.2)
-    print("X Train size:" + str(len(x_train)))
-    print("X Test size:" + str(len(x_test)))
-    print("Y Train size:" + str(len(y_train)))
-    print("Y Train size:" + str(len(y_test)))
-    print("Train test split complete. Converting to np arrays and sending to network.")
-    return x_train, x_test, y_train, y_test
-
-
-def process_no_split(large_file_path_to_load, labels_path):
-    print("Loading data from large file...")
-    all_adjusted = np.load(large_file_path_to_load + ".npy")
-    all_labels = np.load(labels_path + ".npy")
-
-    print("Loaded. Scaling data...")
-    scaler = StandardScaler(copy=False)
-    all_scaled = scaler.fit_transform(all_adjusted)
-    print("Scaling complete. Sending to validator...")
-    return all_scaled, all_labels
-
-
-def pad_tweets(all_tweets):
-    all_adjusted = []
-    for tweet in all_tweets:
-        if len(tweet) >  MAX_VECTOR_COUNT:
-            print("Trimming tweet vectors with vector length " + str(len(tweet)))
-            tweet = tweet[:MAX_VECTOR_COUNT]
-            # trim, but it's highly unlikely there are that many tokens
-        else:
-            # we need to add fake empty tokens to pad
-            tweet = pad_array(tweet, MAX_VECTOR_COUNT)
-        all_adjusted.append(tweet)
-    return all_adjusted
-
-
-def pad_array(A, size):
-    t = size - len(A)
-    return np.pad(A, pad_width=(0, t), mode='constant')
-
-
-def pad_tweet_arr(arr):
-    out = []
-    for tweet in arr:
-        if len(tweet) > MAX_VECTOR_COUNT:
-            print("Trimming tweet vectors with vector length " + str(len(tweet)))
-            tweet = tweet[:MAX_VECTOR_COUNT]
-            # trim, but it's highly unlikely there are that many tokens
-        else:
-            # we need to add fake empty tokens to pad
-            needed_empty = MAX_VECTOR_COUNT - len(tweet)
-            tweet = pad_array(tweet, MAX_VECTOR_COUNT)
-        out.append(tweet)
-    return out
-
+import random
+import itertools as IT
 
 def load_chunk(file_path):
     basename = os.path.basename(file_path)
@@ -111,13 +26,11 @@ def load_chunk(file_path):
     tweets = np.load(file_path)
     labels = []
     scaler = joblib.load('scaler.pkl')
-    logging.info(tweets)
     all_scaled = scaler.transform(tweets)
     if is_bot:
         labels = np.ones(len(all_scaled), dtype=int)
     else:
         labels = np.zeros(len(all_scaled), dtype=int)
-    logging.info(all_scaled)
     x_train, x_test, y_train, y_test = train_test_split(all_scaled, labels, shuffle=True, test_size=0.2)
     logging.info("X Train size:" + str(len(x_train)))
     logging.info("X Test size:" + str(len(x_test)))
@@ -125,3 +38,17 @@ def load_chunk(file_path):
     logging.info("Y Train size:" + str(len(y_test)))
 
     return np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
+
+
+
+
+def evenly_spaced(*iterables):
+    """
+    >>> evenly_spaced(range(10), list('abc'))
+    [0, 1, 'a', 2, 3, 4, 'b', 5, 6, 7, 'c', 8, 9]
+    """
+    return [item[1] for item in
+            sorted(IT.chain.from_iterable(
+            zip(IT.count(start=1.0 / (len(seq) + 1),
+                         step=1.0 / (len(seq) + 1)), seq)
+            for seq in iterables))]
--- src/network.py
+++ src/network.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten
 from keras.models import load_model
 from keras.optimizers import SGD
-import itertools as IT
+
 import numpy as np
 import time
 import datetime
@@ -13,6 +13,7 @@ from keras.utils import to_categorical
 import sklearn.metrics as metrics
 from constants import MAX_VECTOR_COUNT, LEARNING_RATE, CHUNKS_PER_ITERATION, PASSES
 import global_processor
+from global_processor import evenly_spaced
 import random
 import logging
 import sys
@@ -55,17 +56,10 @@ def execute_network(x_train, x_test, y_train, y_test, model_name):
                         epochs=2,
                         batch_size=2048,
                         verbose=1)
-    logging.info("Epochs complete. Saving model...")
-    model.save(model_name)
-    logging.info("Model saved. Use predict_on_model() to see cross validation results.")
     return model
 
 
-def predict_on_model(data_path, label_path, model_name):
-    logging.info("Getting cross validation set from FileReader...")
-    x_cv, y_cv = global_processor.process_no_split(data_path, label_path)
-    logging.info("Loading model...")
-    model = load_model(model_name)
+def predict_on_model(model, x_cv, y_cv):
     logging.info("Load complete.")
     # Report Results
     predicted = model.predict(x_cv)
@@ -81,11 +75,12 @@ def predict_on_model(data_path, label_path, model_name):
     acc = metrics.accuracy_score(y_cv, y_pred)
     prec = metrics.precision_score(y_cv, y_pred, average=None)
     rec = metrics.recall_score(y_cv, y_pred, average=None)
-    print(confusion)
-    print(acc)
-    print(rec)
-    print(prec)
-
+    report = metrics.classification_report(y_cv, y_pred)
+    logging.info("Confusion matrix: {0}".format(confusion))
+    logging.info("Accuracy {0}".format(acc))
+    logging.info("Recall {0}".format(rec))
+    logging.info("Precision {0}".format(prec))
+    logging.info("CLASSIFICATION REPORT\n{0}".format(report))
 
 def train_existing_model(model, x_train, x_test, y_train, y_test, model_name, trainCount):
     logging.info("Initializing model using naive file processing.")
@@ -103,87 +98,114 @@ def train_existing_model(model, x_train, x_test, y_train, y_test, model_name, tr
     logging.info("Model saved. Use predict_on_model() to see cross validation results.")
     return [model, new_model_name]
 
-def evenly_spaced(*iterables):
-    """
-    >>> evenly_spaced(range(10), list('abc'))
-    [0, 1, 'a', 2, 3, 4, 'b', 5, 6, 7, 'c', 8, 9]
-    """
-    return [item[1] for item in
-            sorted(IT.chain.from_iterable(
-            zip(IT.count(start=1.0 / (len(seq) + 1),
-                         step=1.0 / (len(seq) + 1)), seq)
-            for seq in iterables))]
 
-def train_network_on_all_data():
-    # base_name, chunk_lower, chunk_higher, fileNo
-    first = True
-    model = None
-    last_model_name = "model_0"
-    training_iteration_count = 0
-    data_dir = input("Input data dir: ")
+def load_validation_data(dir, model_path):
+    x_train = None
+    x_test = None
+    y_train = None
+    y_test = None
+    interleaved_file_list = get_interleaved_file_list(dir)
+    for file in interleaved_file_list:
+        if x_train is None:
+            x_train, x_test, y_train, y_test = global_processor.load_chunk(os.path.join(dir, file))
+        else:
+            loaded_x_train, loaded_x_test, loaded_y_train, loaded_y_test = global_processor.load_chunk(
+                os.path.join(dir, file))
+            x_train = np.concatenate([x_train, loaded_x_train])
+            x_test = np.concatenate([x_test, loaded_x_test])
+            y_train = np.concatenate([y_train, loaded_y_train])
+            y_test = np.concatenate([y_test, loaded_y_test])
+    model = load_model(model_path)
+    y_all = np.concatenate([y_train, y_test])
+    x_all = np.concatenate([x_train, x_test])
+    logging.info("Available samples {0}".format(x_all.size))
+    predict_on_model(model, x_all, y_all)
+
+
+def get_interleaved_file_list(data_dir):
+    """Builds an interleaved file list such that bot entries are evenly spaced"""
     files = os.listdir(data_dir)
     bot_files = [f for f in files if "bot" in f]
     regular_files = [f for f in files if "regular" in f]
     random.shuffle(bot_files)
     random.shuffle(regular_files)
-    len_both = len(bot_files) + len(regular_files)
-    a = [f for f in files if "bot" in f]
-    b = [f for f in files if "regular" in f]
-    logging.info(a)
-    logging.info(b)
-    interleaved_file_list = evenly_spaced(a, b)
+    interleaved_file_list = evenly_spaced(bot_files, regular_files)
     logging.info(interleaved_file_list)
+    return interleaved_file_list
 
 
-    input()
+def train_network_on_all_data(data_dir):
+    # base_name, chunk_lower, chunk_higher, fileNo
+    first = True
+    model = None
+    last_model_name = "model_0"
+    training_iteration_count = 0
+
     x_train = None
     x_test = None
     y_train = None
     y_test = None
     loaded_count = 0  # loads CHUNKS_PER_ITERATION chunks
-    steps_to_save = 0
-    for iteration in range(0, PASSES):
-        for file in files:
-            if x_train is None:
-                x_train, x_test, y_train, y_test = global_processor.load_chunk(os.path.join(data_dir, file))
+    interleaved_file_list = get_interleaved_file_list()
+    for file in interleaved_file_list:
+        if x_train is None:
+            x_train, x_test, y_train, y_test = global_processor.load_chunk(os.path.join(data_dir, file))
+        else:
+            loaded_x_train, loaded_x_test, loaded_y_train, loaded_y_test = global_processor.load_chunk(
+                os.path.join(data_dir, file))
+            x_train = np.concatenate([x_train, loaded_x_train])
+            x_test = np.concatenate([x_test, loaded_x_test])
+            y_train = np.concatenate([y_train, loaded_y_train])
+            y_test = np.concatenate([y_test, loaded_y_test])
+        loaded_count += 1
+        if loaded_count >= CHUNKS_PER_ITERATION:
+            if first:
+                model = execute_network(
+                    x_train,
+                    x_test,
+                    y_train,
+                    y_test,
+                    last_model_name)
+                first = False
             else:
-                loaded_x_train, loaded_x_test, loaded_y_train, loaded_y_test = global_processor.load_chunk(
-                    os.path.join(data_dir, file))
-                x_train = np.concatenate([x_train, loaded_x_train])
-                x_test = np.concatenate([x_test, loaded_x_test])
-                y_train = np.concatenate([y_train, loaded_y_train])
-                y_test = np.concatenate([y_test, loaded_y_test])
-            loaded_count += 1
-            if loaded_count >= CHUNKS_PER_ITERATION:
-                if first:
-                    model = execute_network(
-                        x_train,
-                        x_test,
-                        y_train,
-                        y_test,
-                        last_model_name)
-                    first = False
-                else:
-                    [model, last_model_name] = train_existing_model(
-                        model,
-                        x_train,
-                        x_test,
-                        y_train,
-                        y_test,
-                        last_model_name,
-                        training_iteration_count)
-                    training_iteration_count += 1
-                # reset arrs
-                loaded_count = 0
-                steps_to_save += 1
-                x_train = None
-                x_test = None
-                y_train = None
-                y_test = None
-        logging.info("Saving model with name {0}.".format(last_model_name))
-        model.save(last_model_name)
+                [model, last_model_name] = train_existing_model(
+                    model,
+                    x_train,
+                    x_test,
+                    y_train,
+                    y_test,
+                    last_model_name,
+                    training_iteration_count)
+                training_iteration_count += 1
+            # reset arrs
+            loaded_count = 0
+            logging.info("Saving model with name {0}.".format(last_model_name))
+            model.save(last_model_name)
+            x_train = None
+            x_test = None
+            y_train = None
+            y_test = None
+
     logging.info("All processing complete! Completed " + str(training_iteration_count) + " iterations.")
 
 
+def menu():
+    """generic menu harness"""
+    while True:
+        print("1: Train network on folder")
+        print("2: Validate on data")
+        print("3: Exit")
+        choice = input("Select option: ")
+        if choice == "1":
+            data_dir = input("Input data dir: ")
+            train_network_on_all_data(data_dir)
+        elif choice == "2":
+            model_path = input("Enter path to model: ")
+            validation_data_dir = input("Enter path to validation data directory: ")
+            load_validation_data(validation_data_dir, model_path)
+        elif choice == "3":
+            exit(0)
+
+
 if __name__ == "__main__":
-    train_network_on_all_data()
+    menu()
